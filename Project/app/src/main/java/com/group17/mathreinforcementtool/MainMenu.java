711fa2e8d85017a126878388d80be35a3d0307e0
joeylombardo <71784614+joeylombardo@users.noreply.github.com>
2020-10-26 18:01:12 -0400

    Main Menu Rough Non-MC Addition

--- Project/app/src/main/java/com/group17/mathreinforcementtool/MainMenu.java
+++ Project/app/src/main/java/com/group17/mathreinforcementtool/MainMenu.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -22,5 +23,25 @@ public class MainMenu extends AppCompatActivity {
     public void onClickMultipleChoice(View view){
         Intent intent = new Intent(this, multipleChoice.class);
         startActivity(intent);
+    }
+
+    /*
+    ----------------------------------------------------
+    Parameters:   v (View)
+    Return:       None
+    Description:  -Called when the buttons related to non-MC Addition are clicked
+                  -Records difficulty ('Easy', 'Medium', 'Hard') the user chose and
+                  switches to MathTemplateActivity.
+                  -!! Although startActivityForResult() is called,
+                  onActivityResult() is not yet implemented.
+    ----------------------------------------------------
+    */
+    public void onAdditionClick(View v)
+    {
+        Button currentButton = findViewById(v.getId());
+        String buttonText = currentButton.getText().toString();
+        Intent i = new Intent(this, MathTemplateActivity.class);
+        i.putExtra("AdditionDifficulty", buttonText);
+        startActivityForResult(i, 10);
     }
 }
